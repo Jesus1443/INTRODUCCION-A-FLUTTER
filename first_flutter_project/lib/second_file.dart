8b7353bfc77035687cd404eeb6ee29b6907fd9d1
JesusR <jesusrm15gg@outlook.com>
2026-04-26 12:25:16 -0600

    Agregando logica al diseño generando numeros aleatorios entre el 0 y 20

--- first_flutter_project/lib/second_file.dart
+++ first_flutter_project/lib/second_file.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SecondClass extends StatelessWidget {
@@ -7,8 +9,14 @@ class SecondClass extends StatelessWidget {
     return Material(
           color: Colors.yellow,
           child: Center(
-            child: Text('This is it.', textDirection: TextDirection.ltr,)
+            child: Text(generateNumbers(), textDirection: TextDirection.ltr,)
           )
         );
+  }
+
+  String generateNumbers() {
+    var r = Random();
+    int i=r.nextInt(20);
+    return 'A random number between 0 and 20 is ${i}';
   }
 }
