ff4e1675a94f76f973e9eb82c85201ff64a06897
JesusR <jesusrm15gg@outlook.com>
2026-04-26 12:08:15 -0600

    Creación de clases para organizar el codigo

--- first_flutter_project/lib/main.dart
+++ first_flutter_project/lib/main.dart
@@ -1,18 +1,22 @@
+import 'package:first_flutter_project/second_file.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
+void main() =>  runApp( MyClass());
+
+
+class MyClass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My flutter app',
       home: Scaffold(
-        backgroundColor: Colors.yellow,
         appBar: AppBar(
           title: Text('title'),
         ),
-        body: Center(
-          child: Text('This is a text.', textDirection: TextDirection.ltr,)
-        ),
+        body: SecondClass() 
       )
-    ),
-  );
+    );
+  }
 }
