b4a254ec0697b4c4bcf1bf9f8c049cf4606ab422
JesusR <jesusrm15gg@outlook.com>
2026-05-02 10:57:03 -0600

    añadiendo Colors, Container, Gesture Detectors, Snackbar, Gradients

--- first_flutter_project/lib/second_file.dart
+++ first_flutter_project/lib/second_file.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:first_flutter_project/util.dart';
 import 'package:flutter/material.dart';
 
 class SecondClass extends StatelessWidget {
@@ -8,9 +9,31 @@ class SecondClass extends StatelessWidget {
 
     return Material(
           color: Colors.yellow,
-          child: Center(
-            child: Text(generateNumbers(), textDirection: TextDirection.ltr,)
+          child: Container(
+            height: 500.0,
+            width: 330.0,
+            alignment: Alignment(0,0),
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                colors: [Colors.green, Colors.blue, Colors.orange, Colors.pink],
+                stops: [0.2, 0.5, 0.7, 1.0],
+                center: Alignment(0.1, 0.3),
+                focal: Alignment(-0.1, 0.6),
+              )
+            ),
+            //color: lightPurpleColor, 
+            child: GestureDetector(
+              onTap: (){
+                ScaffoldMessenger.of(context).showSnackBar(new SnackBar(
+                  content: new Text('It was pressed'),
+                  duration: Duration(seconds: 2),
+                ));
+              },
+              child: Text(generateNumbers(), textDirection: TextDirection.ltr,)
+            ) 
+            
           )
+          
         );
   }
 
